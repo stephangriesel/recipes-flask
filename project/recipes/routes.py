d5b88e483d72232a15d9ebd4f0bcd313c5bca6b1
Patrick Kennedy <pkennedy@hey.com>
2021-01-02 14:35:24 -0800

    Added breakfast listing of recipes; Added pancakes recipe

--- project/recipes/routes.py
+++ project/recipes/routes.py
@@ -5,3 +5,13 @@ from flask import render_template, redirect, url_for
 @recipes_blueprint.route('/')
 def recipes():
     return render_template('recipes/recipes.html')
+
+
+@recipes_blueprint.route('/breakfast')
+def breakfast_recipes():
+    return render_template('recipes/breakfast.html')
+
+
+@recipes_blueprint.route('/breakfast/pancakes')
+def breakfast_recipe_pancake():
+    return render_template('recipes/recipe_details.html')
