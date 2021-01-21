164b90b5f03c8e614affd976c31ce302434790ff
Patrick Kennedy <pkennedy@hey.com>
2021-01-20 21:53:48 -0800

    Added baked goods recipes

--- project/recipes/routes.py
+++ project/recipes/routes.py
@@ -5,6 +5,7 @@ from flask import render_template, abort
 breakfast_recipes_names = ['pancakes', 'acai_bowl', 'honey_bran_muffins', 'breakfast_scramble',
                            'pumpkin_donuts', 'waffles', 'omelette']
 dinner_recipes_names = ['steak_fajitas', 'ground_beef_tacos', 'pizza']
+baked_goods_recipes_names = ['bagels', 'french_bread', 'pitas']
 
 
 @recipes_blueprint.route('/')
@@ -33,6 +34,19 @@ def dinner_recipes():
 @recipes_blueprint.route('/dinner/<recipe_name>/')
 def dinner_recipe(recipe_name):
     if recipe_name not in dinner_recipes_names:
+        abort(404)
+
+    return render_template(f'recipes/{recipe_name}.html')
+
+
+@recipes_blueprint.route('/baked_goods/')
+def baked_goods_recipes():
+    return render_template('recipes/baked_goods.html')
+
+
+@recipes_blueprint.route('/baked_goods/<recipe_name>/')
+def baked_goods_recipe(recipe_name):
+    if recipe_name not in baked_goods_recipes_names:
         abort(404)
 
     return render_template(f'recipes/{recipe_name}.html')
